8072de177f112913d49a9c35314f3da20986f7c7
Khushi <khushibha05@gmail.com>
2026-05-12 22:47:29 +0530

    Corrected information

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,10 +2,7 @@ export const personalData = {
   name: "Khushi Bhatia",
   profile: '/profile2.png',
   designation: "a student, AI/ML enthusiast, and cloud computing learner",
-  description: `My name is Khushi Bhatia. I am a Computer Science student with a strong interest in AI/ML, cloud computing, and software development. I enjoy building creative and practical tech solutions while constantly exploring new technologies and improving my skills.
-I am passionate about problem-solving, teamwork, and bringing ideas to life through technology. From developing web applications and dashboards to working on AI-based investigation and analytics projects, I love combining innovation with real-world impact.
-Alongside technical skills, I enjoy leadership, event management, and collaborative environments that challenge me to grow both personally and professionally.
-I am always eager to learn, take on new opportunities, and contribute to meaningful projects that create value and solve real problems.`,
+  description: `My name is Khushi Bhatia. I am a Computer Science student with interests in AI/ML, cloud computing, and software development. I enjoy building practical tech solutions, solving problems, and exploring new technologies. Along with technical skills, I am passionate about leadership, teamwork, and event management. I enjoy collaborative environments that challenge me to grow personally and professionally while contributing to meaningful and impactful projects.`,
   email: 'khushibha05@gmail.com',
   phone: '8285267666',
   address: 'Gurgaon, Haryana, India',
