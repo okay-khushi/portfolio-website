50b5626d6e7fdb42778b8a2708752ca7e1686e69
Khushi <khushibha05@gmail.com>
2026-05-12 14:28:33 +0530

    Updated navbar and fixed animations

--- app/blog/page.js
+++ app/blog/page.js
@@ -31,7 +31,7 @@ async function page() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
         {
-          blogs.map((blog, i) => (
+          blogs.filter(blog => !blog.title.toLowerCase().includes('build an awesome developer portfolio website')).map((blog, i) => (
             blog?.cover_image &&
             <BlogCard blog={blog} key={i} />
           ))
